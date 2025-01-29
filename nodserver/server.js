6a2932ac0e31075f30d97f9bc534548b45049640
Amy Kopp <amy.kopp@hakweiz.at>
2025-01-29 11:13:40 +0100

    server

--- nodserver/server.js
+++ nodserver/server.js
@@ -104,7 +104,36 @@ app.post('/tbl_media', (req, res) => {
     });
   });
 });
+app.post('/login', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+  const { username, password } = req.body;
+  const query = 'SELECT userID, email, username, password FROM tbl_user WHERE username = ?';
+  db.query(query, [username], (err, results) => {
+    if (err) {
+      console.error('Fehler beim Abrufen der Daten:', err);
+      res.status(500).json({ message: 'Fehler beim Abrufen der Daten.' });
+      return;
+    }
+    if (results.length === 0) {
+      res.status(401).json({ message: 'Benutzername nicht gefunden.' });
+      return;
+    }
+    const user = results[0];
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        console.error('Fehler beim Vergleichen der Passwörter:', err);
+        res.status(500).json({ message: 'Fehler beim Vergleichen der Passwörter.' });
+        return;
+      }
+      if (!isMatch) {
+        res.status(401).json({ message: 'Falsches Passwort.' });
+        return;
+      }
+      res.status(200).json({ message: 'Login erfolgreich.', id: user.userID, username: user.username , email: user.email});
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
-});
+});});
